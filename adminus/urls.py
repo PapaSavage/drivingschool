e7858fc96597195c6392e7f6ef46faf60fdc7e3d
Falaleev Danil <usr23653@vyatsu.ru>
2023-12-24 19:36:15 +0300

    create tab students

--- adminus/urls.py
+++ adminus/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path("", views.home, name="admin_home"),
     path("profile", views.profile, name="admin_profile"),
     path("instructors", views.instructors, name="admin_instructors"),
-    path("lectures", views.home, name="admin_lectures"),
-    path("students", views.home, name="admin_students"),
+    path("lectures", views.lectures, name="admin_lectures"),
+    path("students", views.students, name="admin_students"),
     path("groups", views.home, name="admin_groups"),
     # path("", views.home, name="home"),
 ]
--- adminus/views.py
+++ adminus/views.py
@@ -4,10 +4,10 @@ from django.db import connection
 from django.contrib.auth.decorators import login_required
 import sys
 from django.contrib.auth.models import User
-
-sys.path.append(r"D:\Projects\drivingschool")
 from users.models import Users
 from usertype import *
+
+sys.path.append(r"D:\Projects\drivingschool")
 
 links_admin_home = [
     {
@@ -101,6 +101,70 @@ links_admin_instructors = [
     {
         "url": "admin_students",
         "name_block": "link-body-emphasis",
+        "title": "Студенты",
+        "icon_class": "fas fa-user-graduate fa-fw me-1",
+    },
+]
+links_admin_lectures = [
+    {
+        "url": "admin_home",
+        "name_block": "link-body-emphasis",
+        "title": "Главная",
+        "icon_class": "fas fa-house-user fa-fw me-1",
+    },
+    {
+        "url": "admin_profile",
+        "name_block": "link-body-emphasis",
+        "title": "Личный кабинет",
+        "icon_class": "fas fa-child fa-fw me-1",
+    },
+    {
+        "url": "admin_instructors",
+        "name_block": "link-body-emphasis",
+        "title": "Инструкторы",
+        "icon_class": "fas fa-biking fa-fw me-1",
+    },
+    {
+        "url": "admin_lectures",
+        "name_block": "active",
+        "title": "Лекторы",
+        "icon_class": "fas fa-chalkboard-teacher fa-fw me-1",
+    },
+    {
+        "url": "admin_students",
+        "name_block": "link-body-emphasis",
+        "title": "Студенты",
+        "icon_class": "fas fa-user-graduate fa-fw me-1",
+    },
+]
+links_admin_students = [
+    {
+        "url": "admin_home",
+        "name_block": "link-body-emphasis",
+        "title": "Главная",
+        "icon_class": "fas fa-house-user fa-fw me-1",
+    },
+    {
+        "url": "admin_profile",
+        "name_block": "link-body-emphasis",
+        "title": "Личный кабинет",
+        "icon_class": "fas fa-child fa-fw me-1",
+    },
+    {
+        "url": "admin_instructors",
+        "name_block": "link-body-emphasis",
+        "title": "Инструкторы",
+        "icon_class": "fas fa-biking fa-fw me-1",
+    },
+    {
+        "url": "admin_lectures",
+        "name_block": "link-body-emphasis",
+        "title": "Лекторы",
+        "icon_class": "fas fa-chalkboard-teacher fa-fw me-1",
+    },
+    {
+        "url": "admin_students",
+        "name_block": "active",
         "title": "Студенты",
         "icon_class": "fas fa-user-graduate fa-fw me-1",
     },
@@ -207,7 +271,9 @@ def instructors(request):
                     car_id = row[0]
 
                     query = "Update Cars Set Model = %s, Color_auto = %s, Reg_number_auto = %s where ID_car = %s"
-                    vals
+                    vals = (modelcar, colorcar, reg_number_auto, car_id)
+                    cursor.execute(query, vals)
+
                     connection.commit()
                 messages.success(request, f"Изменения успешно сохранены.")
                 return redirect("admin_instructors")
@@ -215,6 +281,217 @@ def instructors(request):
                 messages.error(request, f"Ошибка сохранения данных.")
 
         return render(request, "adminus/instructors.html", context=data)
+    else:
+        messages.error(request, f"У вас нет доступа к этой ссылке")
+        return redirect("user_home")
+
+
+@login_required
+def lectures(request):
+    if admin_exist(request):
+        data = {"links": links_admin_lectures}
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT Users.ID_user, CONCAT(Users.Surname, ' ', Users.Name, ' ', Users.SecondName) as FIO, Users.Telephone as Telephone, Users.Surname, Users.Name, Users.SecondName, Users.Adress, Users.DateOfBirth, Users.Passport, Users.SeriaMedicalCertificate, Users.Email FROM Lectures JOIN Users on Lectures.ID_lecture = Users.ID_user"
+            )
+            rows = cursor.fetchall()
+            lectures_data = []
+            for row in rows:
+                lectures_data.append(row)
+            cursor.execute("Select Count(*) from Lectures")
+            row = cursor.fetchone()
+            count = row[0]
+            lectr = {
+                "lectures": lectures_data,
+                "count": count,
+            }
+
+        data = data | lectr
+
+        if request.method == "POST":
+            login = request.POST.get("login")
+            password = request.POST.get("password")
+
+            lect_id = request.POST.get("lect_id")
+
+            surname = request.POST.get("surname")
+            firstname = request.POST.get("firstname")
+            secondname = request.POST.get("secondname")
+            adress = request.POST.get("adress")
+            phone = request.POST.get("phone")
+            date = request.POST.get("date")
+            passport = request.POST.get("passport")
+            medicalcertificate = request.POST.get("medicalcertificate")
+            email = request.POST.get("email")
+
+            if login:
+                user = User.objects.create_user(login, email, password)
+                user.save()
+
+            try:
+                with connection.cursor() as cursor:
+                    if login:
+                        query = "SELECT id FROM auth_user order by id desc limit 1"
+                        cursor.execute(query)
+                        row = cursor.fetchone()
+                        lect_id = row[0]
+
+                        query = f"Insert into Lectures (ID_lecture) VALUES ({lect_id})"
+                        cursor.execute(query)
+
+                    query = "Update Users Set Surname = %s, Name = %s, SecondName = %s, Adress = %s, Telephone = %s, DateOfBirth = %s, Passport = %s, SeriaMedicalCertificate = %s, email = %s where id_user = %s"
+                    vals = (
+                        surname,
+                        firstname,
+                        secondname,
+                        adress,
+                        phone,
+                        date,
+                        passport,
+                        medicalcertificate,
+                        email,
+                        lect_id,
+                    )
+                    cursor.execute(query, vals)
+                    connection.commit()
+                messages.success(request, f"Изменения успешно сохранены.")
+                return redirect("admin_lectures")
+            except:
+                messages.error(request, f"Ошибка сохранения данных.")
+
+        return render(request, "adminus/lectures.html", context=data)
+    else:
+        messages.error(request, f"У вас нет доступа к этой ссылке")
+        return redirect("user_home")
+
+
+@login_required
+def students(request):
+    if admin_exist(request):
+        data = {"links": links_admin_students}
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """SELECT 
+    Users.ID_user, 
+    CONCAT(Users.Surname, ' ', Users.Name, ' ', Users.SecondName) as FIO, 
+    Users.Telephone as Telephone, 
+    Groupes.Name as GroupName, 
+    DrivingCategories.Name as CategoryName, 
+    PlansEducations.Name as EducationPlan, 
+    Cars.Model,
+    CONCAT(InstructorUsers.Surname, ' ', InstructorUsers.Name, ' ', InstructorUsers.SecondName) as InstructorFIO,
+    Users.Surname,
+    Users.Name,
+    Users.SecondName,
+    Users.Adress,
+    Users.Telephone,
+    Users.DateOfBirth,
+    Users.Passport,
+    Users.SeriaMedicalCertificate,
+    Users.email
+FROM 
+    Students 
+LEFT JOIN Users ON Students.ID_student = Users.ID_user 
+LEFT JOIN Contracts ON Students.ID_student = Contracts.ID_student 
+LEFT JOIN Groupes ON Contracts.ID_group = Groupes.ID_group 
+LEFT JOIN PlansEducations ON Contracts.ID_plan = PlansEducations.ID_plan 
+LEFT JOIN DrivingCategories ON PlansEducations.ID_category = DrivingCategories.ID_category 
+LEFT JOIN Instructors ON Instructors.ID_instructor = Contracts.ID_instructor 
+LEFT JOIN Cars ON Instructors.ID_car = Cars.ID_car
+LEFT JOIN Users AS InstructorUsers ON Instructors.ID_instructor = InstructorUsers.ID_user;
+"""
+            )
+            rows = cursor.fetchall()
+            students_data = []
+            for row in rows:
+                students_data.append(row)
+
+            cursor.execute("Select Count(*) from Students")
+            row = cursor.fetchone()
+            count = row[0]
+
+            cursor.execute(
+                "Select Instructors.ID_Instructor, CONCAT(Users.Surname, ' ', Users.Name, ' ', Users.SecondName) as InstructorFIO from Instructors join Users on Instructors.ID_instructor = Users.ID_user;"
+            )
+
+            rows = cursor.fetchall()
+            instructors_data = []
+            for row in rows:
+                instructors_data.append(row)
+
+            studs = {
+                "students": students_data,
+                "count": count,
+                "instructors": instructors_data,
+            }
+
+        data = data | studs
+
+        if request.method == "POST":
+            login = request.POST.get("login")
+            password = request.POST.get("password")
+
+            stud_id = request.POST.get("stud_id")
+
+            surname = request.POST.get("surname")
+            firstname = request.POST.get("firstname")
+            secondname = request.POST.get("secondname")
+            adress = request.POST.get("adress")
+            phone = request.POST.get("phone")
+            date = request.POST.get("date")
+            passport = request.POST.get("passport")
+            medicalcertificate = request.POST.get("medicalcertificate")
+            email = request.POST.get("email")
+
+            inst_id = request.POST.get("inst_id")
+
+            if login:
+                user = User.objects.create_user(login, email, password)
+                user.save()
+
+            try:
+                with connection.cursor() as cursor:
+                    if login:
+                        query = "SELECT id FROM auth_user order by id desc limit 1"
+                        cursor.execute(query)
+                        row = cursor.fetchone()
+                        stud_id = row[0]
+
+                        query = f"Insert into Students (ID_student) VALUES ({stud_id})"
+                        cursor.execute(query)
+
+                    query = "Update Users Set Surname = %s, Name = %s, SecondName = %s, Adress = %s, Telephone = %s, DateOfBirth = %s, Passport = %s, SeriaMedicalCertificate = %s, email = %s where id_user = %s"
+                    vals = (
+                        surname,
+                        firstname,
+                        secondname,
+                        adress,
+                        phone,
+                        date,
+                        passport,
+                        medicalcertificate,
+                        email,
+                        stud_id,
+                    )
+                    cursor.execute(query, vals)
+
+                    query = (
+                        "Update Contracts SET ID_instructor = %s WHERE ID_student = %s"
+                    )
+                    vals = (inst_id, stud_id)
+                    cursor.execute(query, vals)
+
+                    print(stud_id)
+
+                    connection.commit()
+                messages.success(request, f"Изменения успешно сохранены.")
+                return redirect("admin_students")
+            except:
+                messages.error(request, f"Ошибка сохранения данных.")
+
+        return render(request, "adminus/students.html", context=data)
     else:
         messages.error(request, f"У вас нет доступа к этой ссылке")
         return redirect("user_home")
